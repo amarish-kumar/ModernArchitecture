1718e5bd617652c3efa8d150a498436fab2de662
Tymofii Bondar <tbondar@thinksmart.com>
2017-10-26 17:08:25 +0300

    Added SQL Project

---
+++ MA.Web/MA.DB/dbo/Tables/User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User] (
+    [ID]       UNIQUEIDENTIFIER NOT NULL,
+    [IsActive] BIT              NOT NULL,
+    [Username] NVARCHAR (128)   NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
